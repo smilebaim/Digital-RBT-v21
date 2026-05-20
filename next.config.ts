481217223fd38c59c825e0b63d3cff1a2dbad111
Maintenance Web <maintenance.web@gmail.com>
2026-05-20 12:51:28 +0700

    ok
    ok

---
+++ next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  // Allow loading external scripts (CDN)
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
